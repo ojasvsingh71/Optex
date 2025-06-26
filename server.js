78ae1a3734fd6ce7a57d022674dddb3de380c3fa
ojasvsingh71 <ojasvsingh0@gmail.com>
2025-06-26 17:53:48 +0530

    cors added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import AIrouter from "./routes/ai.route.js";
 import connectDB from "./lib/connectDB.js";
+import cors from "cors"
 
 dotenv.config();
 connectDB();
@@ -9,6 +10,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/", AIrouter);
 
